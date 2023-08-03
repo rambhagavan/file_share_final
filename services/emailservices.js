2943393800fd73a33ace50810c50d2f8aa6b342a
rambhagavan <shivaramyadav12@gmail.com>
2023-08-03 23:26:12 +0530

    email

--- services/emailservices.js
+++ services/emailservices.js
@@ -4,11 +4,8 @@ require("dotenv/config");
 async function sendMail({from,to,subject,text,html}){
 let transporter=nodemailer.createTransport({
     host:process.env.SMTP_HOST,
-<<<<<<< HEAD
     port:process.env.SMTP_PORT ,
-=======
     port:process.env.SMTP_PORT ||587,
->>>>>>> 2e87bbc0cb78c0cb6117b89805fc31b17312763d
     secure:false,
     auth:{
         user:process.env.MAIL_USER,
